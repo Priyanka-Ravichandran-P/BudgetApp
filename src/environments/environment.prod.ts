12b3754586f15616fb6e4d41d2ede76893c8bd22
Priyanka-Ravichandran-P <priya.r6907@gmail.com>
2021-10-20 14:44:33 +0530

    connected to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBJR_3Y6Yv_rYGHUw8-Y978bClJHlH7sSk",
+    authDomain: "budget-calculator-e5385.firebaseapp.com",
+    projectId: "budget-calculator-e5385",
+    storageBucket: "budget-calculator-e5385.appspot.com",
+    messagingSenderId: "1054598876469",
+    appId: "1:1054598876469:web:b00f4b9d28ef3600d1907c",
+    measurementId: "G-3XWS6LZ44K"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBJR_3Y6Yv_rYGHUw8-Y978bClJHlH7sSk",
+    authDomain: "budget-calculator-e5385.firebaseapp.com",
+    projectId: "budget-calculator-e5385",
+    storageBucket: "budget-calculator-e5385.appspot.com",
+    messagingSenderId: "1054598876469",
+    appId: "1:1054598876469:web:b00f4b9d28ef3600d1907c",
+    measurementId: "G-3XWS6LZ44K"
+  }
 };
 
 /*
